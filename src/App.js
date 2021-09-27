dc829b2375f8d9b0e76eab53623eff987ea1108f
Warren Pistorius <warren.pistorius@sortedservices.com>
2021-09-27 14:37:44 +1000

    added cart context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   
@@ -15,13 +16,13 @@ function App() {
   }
   
   return (
-    <Fragment>
+    <CartProvider>
       {cartVisible && <Cart showCart={showCartHandler} />}
       <Header showCart={showCartHandler}   />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import CardContext from "./cart-context";
+
+const CartProvider = props => {
+    
+    const addItemHandler = item =>{};
+
+    const removeItemHandler = id => {
+
+    }
+    
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemHandler,
+        removeItem: removeItemHandler
+    }
+    
+    return <CardContext.Provider value={cartContext}>
+        {props.children}
+    </CardContext.Provider>
+};
+
+export default CartProvider;
